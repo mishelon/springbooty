dc2b4ac8b275b872467301b2d0dcf65af968033b
mishelon <mishelon@gmail.com>
2024-07-11 18:03:44 +0200

    some validations
    some validations

--- demo/src/main/java/com/mishelon/base/dto/UserDTO.java
+++ demo/src/main/java/com/mishelon/base/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.mishelon.base.dto;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,14 +15,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
-  @Size(min = 4, max = 15, message = "{user.email.validation}")
   @Email
+  @NotNull(message = "{user.email.notnull}")
+  @Size(min = 4, max = 15, message = "{user.email.validation}")
   private String email;
   private String id;
+  @NotNull(message = "{user.login.notnull}")
   @Size(min = 4, max = 15, message = "{user.login.validation}")
   private String login;
+  @NotNull(message = "{user.name.notnull}")
   @Size(min = 4, max = 15, message = "{user.name.validation}")
   private String name;
+  @NotNull(message = "{user.surname.notnull}")
   @Size(min = 4, max = 15, message = "{user.surname.validation}")
   private String surname;
 }
